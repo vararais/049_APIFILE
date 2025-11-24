822d3d066ab70ec80ecfc2a0a69992aeca14b8c3
vararais <varadigmaisnann@gmail.com>
2025-11-24 12:34:18 +0700

    Revisi salah layout

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ const express = require('express');
 const connectDatabase = require('./config/db');
 const apiRoute = require('./routes/api');
 
-app.use('/api/v1', apiRoute);
-
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use('/api/v1', apiRoute);
 
 async function startServer() {
     await connectDatabase();
